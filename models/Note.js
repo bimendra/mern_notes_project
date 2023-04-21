96968aecc424c327bb4de719e225cc8540f8c98e
Bimendra Bandara <bimendra.bandara@velaris.io>
2023-04-22 00:48:28 +0800

    Add Mongo db models

---
+++ models/Note.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const AutoIncrmenet = require('mongoose-sequence')(mongoose);
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+noteSchema.plugin(AutoIncrmenet, {
+  inc_field: 'ticket',
+  id: 'ticketNums',
+  starts_seq: 500,
+});
+
+module.exports = mongoose.model('Note', noteSchema);
